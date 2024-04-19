bfa926681c19314b5eca096fb0c041a1665c3935
jacobyi0730 <jacobyi2193@gmail.com>
2024-04-20 03:50:00 +0900

    13_플레이어 애니메이션 적용

---
+++ Source/Cpp_TSP/Private/TPSPlayerAnim.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSPlayerAnim.h"
+#include "TPSPlayer.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UTPSPlayerAnim::NativeUpdateAnimation( float DeltaSeconds )
+{
+	// Speed, Direction, IsInAir
+	ATPSPlayer* player =  Cast<ATPSPlayer>(TryGetPawnOwner());
+	if (player)
+	{
+		FVector velocity = player->GetVelocity();
+		FVector forward = player->GetActorForwardVector();
+		FVector right = player->GetActorRightVector();
+
+		Speed = FVector::DotProduct( forward , velocity );
+		Direction = FVector::DotProduct( right , velocity );
+
+		IsInAir = player->GetCharacterMovement()->IsFalling();
+	}
+}
---
+++ Source/Cpp_TSP/Public/TPSPlayerAnim.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "TPSPlayerAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_TSP_API UTPSPlayerAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	 virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+	 UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	 float Speed;
+
+	 UPROPERTY( EditAnywhere , BlueprintReadWrite )
+	 float Direction;
+
+	 UPROPERTY( EditAnywhere , BlueprintReadWrite )
+	 bool IsInAir;
+
+};
